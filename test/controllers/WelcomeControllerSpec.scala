d3944d9fd6432ddb23e1d29e8fa79d30eb5cb973
Mike Pratt <mike.pratt@digital.hmrc.gov.uk>
2017-06-06 11:12:14 +0100

    Added welcome.scala.html and routes

--- test/controllers/WelcomeControllerSpec.scala
+++ test/controllers/WelcomeControllerSpec.scala
@@ -10,12 +10,37 @@ import play.api.test.Helpers.{status, _}
   */
 class WelcomeControllerSpec extends PlaySpec with GuiceOneAppPerTest {
     "WelcomeController GET" should {
-      "return a successful respinse" in {
+      "return a successful response, return some html and say hello and have a title" in {
         val controller = new WelcomeController
         val result = controller.welcome().apply(FakeRequest(GET, "/foo"))
         status(result) mustBe OK
-      }
+        contentType(result) mustBe Some("text/html")
+        contentAsString(result) must include ("<h1>Hello!</h1>")
+        contentAsString(result) must include ("<title>Welcome!</title>")
 
+      }
+    "respond to the /welcome url" in {
+        val request = FakeRequest(GET, "/welcome").withHeaders("Host" -> "localhost")
+        val home = route(app, request).get
+        status(home) mustBe OK
+}
+      "say hello and have a title" in {
+        val controller = new WelcomeController
+      }
     }
 
+
+/*
+    "return some html" in {
+    val controller = new WelcomeController
+    val result = controller.welcome().apply(FakeRequest(GET, "/foo"))
+    contentType(result) mustBe Some("text/html")
+}
+
+    "say hello and have a title" in {
+    val controller = new WelcomeController
+    val result = controller.welcome().apply(FakeRequest(GET, "/foo"))
+    contentAsString(result) must include ("<h1>Hello!</h1>")
+    contentAsString(result) must include ("<title>Welcome!</title>")
+  }*/
 }
